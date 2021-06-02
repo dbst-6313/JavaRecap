adde4c4eb173e02bcc668bc607717da97f9331f7
dbst-6313 <birisierikli@hotmail.com>
2021-06-02 17:51:22 +0300

    HRMS

--- src/main/java/hrms/recap/dataAccess/abstracts/JobSeekersDao.java
+++
@@ -1,9 +0,0 @@
-package hrms.recap.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hrms.recap.entities.concretes.JobSeeker;
-
-public interface JobSeekersDao extends JpaRepository<JobSeeker,Integer>{
-  
-}
